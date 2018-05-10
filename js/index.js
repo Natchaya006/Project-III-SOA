3e1121b3d669dfc17b71e734bf0621fb47838a83
Natchaya <pinzarz39@hotmail.com>
2018-05-10 13:34:03 +0700

    set format product-catagory page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-
+	//---------------Login/Register-----------------------
 	$('#login-form-link').click(function (e) {
 		$("#login-form").delay(100).fadeIn(100);
 		$("#register-form").fadeOut(100);
@@ -14,5 +14,25 @@ $(function () {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-
+	//----------------------------------------------------
+	//---------------Product Show-------------------------
+	// $.ajax({
+	// 	type: "GET",
+	// 	url: "https://productapi977377.herokuapp.com/Products.php/api/products",
+	// 	dataType: "text",
+	// 	success: function (data) {
+	// 		console.log(data);
+	// 		var obj = JSON.parse(data);
+	// 		console.log(obj);
+	// 		alert(obj);
+	// 	}
+	// });
+	$(".showProduct").mouseover(function () {
+		$(".showProduct").css("background-color", "rgba(255, 255, 255, 0.164)");
+	});
+	$(".showProduct").mouseout(function () {
+		$(".showProduct").css("background-color", "");
+	});
+	$(".showProduct").click(function (e) {});
+	//----------------------------------------------------
 });
