80c09a96b6316151b7723f0efa700879a0bd6db8
Natchaya <pinzarz39@hotmail.com>
2018-05-10 18:56:29 +0700

    get all product show

--- js/index.js
+++ js/index.js
@@ -14,29 +14,88 @@ $(function () {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
+	$("#login-submit").click(function (e) {
+		var usernameInput = $("#username").val();
+		var passwordInput = $("#password").val();
+		var urlGetByEmail = "";
+		$.ajax({
+			type: "GET",
+			url: urlGetByEmail,
+			dataType: "json",
+			success: function (data) {
+				if (data.password == passwordInput) {
+					window.location('product-catagory.html');
+				} else {
+					alert('Username or Password Wrong!');
+					location.reload();
+				}
+			}
+		});
+	});
+	$("#register-submit").click(function (e) {
+		var usernameRegist = $("#username").val();
+		var fitstnameRegist = $("#firstName").val();
+		var passwordRegist = $("#password").val();
+		var addressRegist = $("#address").val();
+		var lastnameRegist = $("#lastName").val();
+		var emailRegist = $("#email").val();
+		var urlPostCustomer = "";
+		$.ajax({
+			type: "POST",
+			url: urlPostCustomer,
+			dataType: "json",
+			data: {
+				/*username: "John",
+				time: "2pm"*/
+			},
+			success: function (data) {
+				if (data.password == passwordInput) {
+					alert("Register Complete!");
+					location.reload();
+				} else {
+					alert('Something Wrong!');
+				}
+			}
+		});
+	});
 	//----------------------------------------------------
 	//---------------Product Show-------------------------
-	// $.ajax({
-	// 	type: "GET",
-	// 	url: "https://productapi977377.herokuapp.com/Products.php/api/products",
-	// 	dataType: "text",
-	// 	success: function (data) {
-	// 		console.log(data);
-	// 		var obj = JSON.parse(data);
-	// 		console.log(obj);
-	// 		alert(obj);
-	// 	}
-	// });
+	$.ajax({
+		type: "GET",
+		url: "https://productapi977377.herokuapp.com/Products.php/api/products",
+		dataType: "text",
+		success: function (data) {
+			var obj = jQuery.parseJSON(data);
+			for (i = 0; i < obj.length; i++) {
+				var id = obj[i].productID;
+				var title = obj[i].title;
+				var img = obj[i].picture;
+				var price = obj[i].price;
+				$('#rowHome').append('<div class="col-lg-3 showProduct"><div id="title">'+
+				'<h3><b><u>'+title+'</u></b></h3></div><div id="pic"><img class="show" src'+
+				'="'+img+'" alt="'+title+'"></div><p></p><div id="desc"><ul class="list-inline">'+
+				'<li><h4>Price : '+price+' bath</h4></li><li><button id="addChart" type="button"'+
+				' value="'+id+'" class="btn .btn-xs btn-success">Buy Now</button></li></ul>'+
+				'</div><div id="review"><ul class="list-inline"><li><span class='+
+				'"glyphicon glyphicon-star"></span><b>Rating</b> : 4/5 </li><li><span class'+
+				'="glyphicon glyphicon-comment"></span><b>Comment</b> : well done</li><li><b>'+
+				'Rating?</b> : <input id="inputRat" type="text">&nbsp;*(0-5)</li><br><p></p><li>'+
+				'<b>Comment?</b> : <input id="inputCom" type="text"></li><li><button id='+
+				'"reviewProduct" type="button" value="'+id+'" class="btn .btn-xs btn-info">Review'+
+				'</button></li></ul></div></div>');
+			}
+		}
+	});
 	$(".showProduct").mouseover(function () {
 		$(".showProduct").css("background-color", "rgba(255, 255, 255, 0.164)");
 	});
 	$(".showProduct").mouseout(function () {
 		$(".showProduct").css("background-color", "");
 	});
-	$("#addChart").click(function (e){
+	$("#addChart").click(function (e) {
 
 	});
-	$("#addChart").click(function (e){
+	$("#addChart").click(function (e) {
 
 	});
 	//----------------------------------------------------
